38abc4b86e1430c1c95a644c570ef130a817f404
haru2036 <haru2036@gmail.com>
2016-03-15 17:08:35 +0900

    add default scotty app

--- Main.hs
+++ Main.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Web.Scotty
+import Model
+import Locman.WebSocket
 
 main :: IO ()
 main = scotty 3000 $ do
   get "/:word" $ do
     beam <- param "word"
     html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+  get "/ws/" $ do
+    runSocket $ User "hoge" "piyo"
