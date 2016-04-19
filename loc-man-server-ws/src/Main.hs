6a50fd0937768e3d79242f2b2a96ce37bcdd3b80
haru2036 <haru2036@gmail.com>
2016-04-19 18:00:00 +0900

    add postgres support

--- loc-man-server-ws/src/Main.hs
+++ loc-man-server-ws/src/Main.hs
@@ -4,16 +4,23 @@ import Network.WebSockets(runServer, acceptRequest, PendingConnection)
 import LocMan.WebSockets(runSockets, joinSession)
 import LocMan.Types
 import LocMan.Model
-import Database.Persist.Sqlite
+import Database.Persist.Postgresql
+import Database.Persist
+import Control.Monad.Logger
+import Control.Monad.IO.Class(liftIO)
 import Control.Concurrent.STM.TVar(newTVar, TVar)
 import Control.Concurrent.STM(atomically)
 import Data.Text ()
 import Data.Map (empty)
 
+connStr = "host=iphoge dbname=test user=postgres password=hoge port=32768"
+
 main :: IO ()
 main = do
-  _ <- runSqlite ":memory:" $ do
-        runMigration migrateAll
+  runStderrLoggingT $ withPostgresqlPool connStr 10 $ \pool -> liftIO $ do
+         flip runSqlPersistMPool pool $ do
+            runMigration migrateAll
+  print "db connection started"
   appStatus <- atomically $ newTVar empty
   let usr = User "stub" Nothing
   sess <- atomically $ do
